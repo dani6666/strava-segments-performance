1b2a3a3d6de7e52dc4a18d9597a6b7d712d6bd48
Daniel Włudarczyk <danielwz99@gmail.com>
2026-06-30 00:06:29 +0200

    fixing db configuration

---
+++ strava-segments-performance-backend/Migrations/20260629220217_ClearUserCredentials.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StravaSegmentsPerformanceBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ClearUserCredentials : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM \"Users\"");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
